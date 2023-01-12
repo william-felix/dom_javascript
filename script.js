436aecd0ac5a85a33e9c3824f4368184f7cf8cfd
william-felix <122409826+william-felix@users.noreply.github.com>
2023-01-11 21:17:37 -0300

    Nova função
    testando descrição

---
+++ script.js
@@ -0,0 +1,16 @@
+var minhaVariavel;
+var variavelTexto = "meu texto";
+var varBooleana = false;
+
+function minhaFuncao(nome){
+
+    alert("alerta 1");
+    alert(nome);
+}
+alert(minhaVariavel);
+minhaVariavel = "Valor definido";
+alert(minhaVariavel);
+minhaVariavel = 150;
+alert(minhaVariavel);
+if(varBooleana){
+    alert("Condição Verdadeira")}
